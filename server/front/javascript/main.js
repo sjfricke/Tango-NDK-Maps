820ad55a9f1ae411ffe5e370175534847694558b
sjfricke <sjfricke@wisc.edu>
2017-04-23 08:53:09 -0600

    first attempt server merge

--- server/front/javascript/main.js
+++ server/front/javascript/main.js
@@ -11,44 +11,19 @@ function init() {
     // creates canavs and scene
     container = document.getElementById( 'container' );
     camera = new THREE.PerspectiveCamera( 27, window.innerWidth / window.innerHeight, 5, 3500 );
-    camera.position.z = 2750;
+    camera.position.z = 1000;
     
     scene = new THREE.Scene();
     scene.fog = new THREE.Fog( 0x050505, 2000, 3500 );
     
-    var particles = 500000;
-    var geometry = new THREE.BufferGeometry();
-    var positions = new Float32Array( particles * 3 );
-    var colors = new Float32Array( particles * 3 );
-    var color = new THREE.Color();
     
-    var n = 1000, n2 = n / 2; // particles spread in the cube
-    for ( var i = 0; i < positions.length; i += 3 ) {
-        // positions
-        var x = Math.random() * n - n2;
-        var y = Math.random() * n - n2;
-        var z = Math.random() * n - n2;
-        positions[ i ]     = x;
-        positions[ i + 1 ] = y;
-        positions[ i + 2 ] = z;
-        // colors
-        var vx = ( x / n ) + 0.5;
-        var vy = ( y / n ) + 0.5;
-        var vz = ( z / n ) + 0.5;
-        color.setRGB( vx, vy, vz );
-        colors[ i ]     = color.r;
-        colors[ i + 1 ] = color.g;
-        colors[ i + 2 ] = color.b;
-    }    
-    
-    geometry.addAttribute( 'position', new THREE.BufferAttribute( positions, 3 ) );
-    geometry.addAttribute( 'color', new THREE.BufferAttribute( colors, 3 ) );
-    geometry.computeBoundingSphere();
-    
-    
-    var material = new THREE.PointsMaterial( { size: 15, vertexColors: THREE.VertexColors } );
-    points = new THREE.Points( geometry, material );
-    scene.add( points );
+    addParticles(
+        {
+            particles : 500000,
+            color : { r : 1, g : 0, b : 0 },
+            size : 1
+        }
+    );
         
     // Render Setup
     renderer = new THREE.WebGLRenderer( { antialias: false } );
@@ -90,4 +65,36 @@ function animate() {
 
 function render() {
     renderer.render( scene, camera );
+}
+
+function addParticles(data) {
+    var particles = data.particles;
+    var geometry = new THREE.BufferGeometry();
+    var positions = new Float32Array( particles * 3 );
+    var colors = new Float32Array( particles * 3 );
+    var color = new THREE.Color();
+    
+    var n = 400, n2 = n / 2; // particles spread in the cube
+    for ( var i = 0; i < positions.length; i += 3 ) {
+        // positions
+        var x = Math.random() * n - n2;
+        var y = Math.random() * n - n2;
+        var z = Math.random() * n - n2;
+        positions[ i ]     = x;
+        positions[ i + 1 ] = y;
+        positions[ i + 2 ] = z;
+        // colors        
+        colors[ i ]     = data.color.r;
+        colors[ i + 1 ] = data.color.g;
+        colors[ i + 2 ] = data.color.b;
+    }    
+    
+    geometry.addAttribute( 'position', new THREE.BufferAttribute( positions, 3 ) );
+    geometry.addAttribute( 'color', new THREE.BufferAttribute( colors, 3 ) );
+    geometry.computeBoundingSphere();
+    
+    
+    var material = new THREE.PointsMaterial( { size: data.size, vertexColors: THREE.VertexColors } );
+    points = new THREE.Points( geometry, material );
+    scene.add( points );
 }
--- server/server.js
+++ server/server.js
@@ -139,3 +139,113 @@ function onListening() {
     : 'port ' + addr.port;
   console.log('Listening on ' + bind);
 }
+
+
+const PORT_NET = 6419;
+const VERBOSE = false;
+const SCALE = 300;
+var net_key = "";
+var net_option = "";
+var net_value = "";
+var nl_1 = -1; //new line 1
+var nl_2 = -1; 
+
+var net_vec = [];
+var data_chunks = ""; // concatination of all chunks
+var chunks = false;
+
+var net = require('net');
+var net_server = net.createServer( (connection) => {
+    console.log('client connected');
+
+    connection.on('close', () => {
+	console.log('client disconnected');
+    });
+
+    connection.on('data', (data) =>  {
+	
+	// getting breaks in protocol
+	//data = data.toString().replace(/\0[\s\S]*$/g,'');
+	data = data.toString();
+	nl_1 = data.indexOf("\n");
+
+	nl_2 = data.substr(nl_1+1).indexOf("\n") + (nl_1 + 1);
+	// getting broken up parts
+	net_key = data.substring(0,nl_1);
+
+	// if nl_1 is -1 then its being sent as data chunks
+	// TODO find why it can start wit a \n
+	if (nl_1 == -1 || net_key.length >  8) {
+	    chunks = true;
+	    data_chunks += data;
+	} else {
+	    	    
+	    net_option = data.substring(nl_1+1, nl_2);
+	    
+	    // if it doesn't null terminate then it would be a empty string
+	    if (net_key == 3) {
+		net_value = data.substr(nl_2+1, data.indexOf("&"));
+	    } else if (data.indexOf("\0") == -1) {
+		net_value = data.substr(nl_2+1);
+	    } else {
+		net_value = data.substring(nl_2+1, data.indexOf("\0"));
+	    }
+	
+	    if ( data.length > 0 && net_key != 1) {
+
+		//check if data was being chunked
+		if (chunks) {
+		    if (VERBOSE) console.log("binding chunks");
+		    chunks = false;
+		    net_value = data_chunks + net_value;
+		    data_chunks = "";
+		}
+
+		// Parse data - Format: X/Y/Z, ( ex: 3.2/4.1/7.5, == x:3.2, y:4.1, z:7.5 )
+		let points = net_value.split(",");
+		for (let i = 0; i < points.length; i++) {
+		    let point = points[i].split("/");
+		    
+		    x_f = parseFloat(point[0]) * SCALE;
+		    y_f = parseFloat(point[1]) * SCALE;
+		    z_f = parseFloat(point[2]) * SCALE;
+
+		    if (x_f == null || y_f == null || z_f == null ||
+		        x_f == NaN  || y_f == NaN  || z_f == NaN ) { continue; } //skip if invalid
+
+		    net_vec.push( { "x" : x_f, "y" : y_f, "z" : z_f } );
+		}
+		console.log("pushed vecs");
+
+		if (net_key == 3) {
+		    console.log("key == 3");
+		    //fs.writeFileSync("test", JSON.stringify(net_vec), 'utf8');
+		    console.log("file write!");
+		    net_vec = [];
+		}
+	    }
+	    
+	    if (VERBOSE) console.log("key: ", net_key, "\toption: ", net_option);
+	    
+	}
+	    
+	connection.write("0"); //Sended response to client
+	
+//	connection.end();
+//	console.log('Disconnected the client.');
+    });
+
+    connection.on('end', () => {
+	console.log('END');
+    });
+
+    connection.on('error', (err) => {
+	console.log('ERROR: ', err);
+    });
+
+});
+
+net_server.listen(PORT_NET, () =>  {
+    console.log('NodeJS Socket Server Ready - Port ' + PORT_NET);
+}); 
+
